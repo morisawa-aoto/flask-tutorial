58da2422ebeea7df5246792249ce0bf375753ce4
morisawa-aoto <toda2020-024@iniad.org>
2021-06-07 12:06:50 +0000

    fortune.html追加

--- app.py
+++ app.py
@@ -10,3 +10,14 @@ def hello():
 		
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+@app.route('/fortune')
+def fortune():
+    unsei_list=["大吉","中吉","小吉"]
+    uranai_list=[
+        {"day":"今日","fortune":random.choice(unsei_list)},
+        {"day":"明日","fortune":random.choice(unsei_list)},
+        {"day":"明後日","fortune":random.choice(unsei_list)}
+    ]
+    return render_template('fortune.html',fortune_list=uranai_list)
